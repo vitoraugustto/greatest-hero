86049d2ad9099fabf9df7d07f0100c95540774de
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-10 23:35:27 -0300

    create get all

--- server/src/routes/personRoutes.js
+++ server/src/routes/personRoutes.js
@@ -7,15 +7,29 @@ router.post('/', async (req, res) => {
     const { name, age } = req.body
     const person = { name, age }
       
-    if (!name || !age) res.status(422).json({ error: 'Name and age are required.' })
+    if (!name || !age) {
+      res.status(422).json({ error: 'Name and age are required.' })
+      return
+    }
       
     try {
       await Person.create(person)
   
-      res.status(201).json({ message: 'Person created successfully' })
-    } catch (err) {
-      res.status(500).json({ error: err })
+      res.status(201).json({ message: 'Person created successfully.' })
+    } catch (error) {
+      res.status(500).json({ error: error })
     }
 })  
+
+router.get('/', async (_, res) => {
+  try {
+    const people = await Person.find()
+
+    res.status(200).json(people)
+  } catch (error)  {
+    res.status(500).json({ error: error })
+  }
+})
+
 
 export default router
