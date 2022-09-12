d31ecf5d83572777eeb280f079aaa1969251c63d
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-12 01:32:27 -0300

    create delete person

--- server/src/routes/personRoutes.js
+++ server/src/routes/personRoutes.js
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
     try {
       await Person.create(person)
   
-      res.status(201).json({ message: 'Person created successfully.' })
+      res.status(201).json(person)
     } catch (error) {
       res.status(500).json({ error: error })
     }
@@ -59,9 +59,28 @@ router.patch('/:id', async (req, res) => {
 
     if (updatedPerson.matchedCount === 0) { 
       res.status(422).json({ message: 'User not found.' })
+      return
     }
 
     res.status(200).json(person)
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id 
+  const person = await Person.findOne({ _id: id })
+
+  if (!person) {
+    res.status(422).json({ message: 'User not found.' })
+    return
+  }
+
+  try {
+    await Person.deleteOne({ _id: id })
+
+    res.status(200).json({ message: 'User deleted successfully.' })
   } catch (error) {
     res.status(500).json({ error: error })
   }
