2e4be1c0047b8d005972cb3b6797929a3cc4c3cf
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-23 02:10:31 -0300

    add /api/v1 to routes

--- server/index.js
+++ server/index.js
@@ -2,18 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 import { DB_PASSWORD, DB_USERNAME } from './.env.js';
+import heroRoutes from './src/routes/heroRoutes.js';
 import itemRoutes from './src/routes/itemRoutes.js';
 
 const app = express();
 const PORT = 8000;
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/item', itemRoutes);
+
+app.use('/api/v1/item', itemRoutes);
+app.use('/api/v1/hero', heroRoutes);
 
 mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@heros-market-cluster.mcfekrd.mongodb.net/?retryWrites=true&w=majority`)
   .then(() => {
