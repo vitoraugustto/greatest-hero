c74ff755261b805f84b435bc9a167c9cd143eae6
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-24 03:10:45 -0300

    create route to store items in inventory

--- server/src/routes/heroRoutes.js
+++ server/src/routes/heroRoutes.js
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/', async (_, res) => {
   try {
-    const hero = await Hero.find();
+    const hero = await Hero.find().select('-inventory');
 
     res.status(200).json(...hero);
   } catch (error) {
@@ -51,9 +51,34 @@ router.patch('/equip-item/:id', async (req, res) => {
         'status.defense': currentDefense + itemDefense,
       },
       { new: true },
-    );
+    ).select('-inventory');
 
     res.status(200).json(updatedHero);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+router.post('/inventory/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const item = await Item.findOne({ _id: id });
+    const hero = await Hero.findOne();
+
+    hero.inventory.forEach((inventoryItem) => {
+      if (String(inventoryItem._id) === id) {
+        res.status(400).json({ error: 'Item already stored.' });
+      }
+    });
+
+    const updatedHero = await Hero.findOneAndUpdate(
+      {},
+      { inventory: [...hero.inventory, item] },
+      { new: true },
+    );
+
+    res.status(200).json(updatedHero.inventory);
   } catch (error) {
     res.status(500).json({ error });
   }
