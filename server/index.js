0a0c83f64e7617f9b119c2a75701400466f25dd8
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-24 11:48:43 -0300

    rename from itemRoutes to itemsRoutes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 import { DB_PASSWORD, DB_USERNAME } from './.env.js';
 import heroRoutes from './src/routes/heroRoutes.js';
-import itemRoutes from './src/routes/itemRoutes.js';
+import itemsRoutes from './src/routes/itemsRoutes.js';
 
 const app = express();
 const PORT = 8000;
@@ -11,7 +11,7 @@ const PORT = 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api/v1/item', itemRoutes);
+app.use('/api/v1/items', itemsRoutes);
 app.use('/api/v1/hero', heroRoutes);
 
 mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@heros-market-cluster.mcfekrd.mongodb.net/?retryWrites=true&w=majority`)
