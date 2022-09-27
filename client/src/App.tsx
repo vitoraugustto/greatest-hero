cdd07c7697b5e61b421570d9740a9f4dfee4e7da
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-27 13:09:16 -0300

    debug vite env in production

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
+
+  console.log(import.meta.env.VITE_BACKEND_URL)
 
   return (
     <div className="App">
