299d88a614fec70eb4225091f6ecf4a605705d0b
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-28 16:56:06 -0300

    create Background component

---
+++ client/src/components/Layout/Background.tsx
@@ -0,0 +1,27 @@
+import { CSSProperties } from 'react';
+import styled from 'styled-components';
+
+import Spacer from './Spacer';
+
+interface IBackground {
+  style?: CSSProperties;
+  children: React.ReactElement | React.ReactElement[];
+}
+
+const Background: React.FC<IBackground> = ({ children }) => {
+  return (
+    <StyledDiv>
+      <Spacer p={16}>{children}</Spacer>
+    </StyledDiv>
+  );
+};
+
+const StyledDiv = styled.div<IBackground>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #332e5b;
+  min-height: 100vh;
+`;
+
+export default Background;
