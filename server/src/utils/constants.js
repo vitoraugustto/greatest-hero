3a0d4bcb2ee74a0974a6d0919015f460249e7ed6
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-28 19:52:11 -0300

    rename from heros-market to greatest-hero

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -4,5 +4,5 @@ export const ENVIRONMENT = NODE_ENV || 'development';
 
 export const CLIENT_URL =
   ENVIRONMENT === 'production'
-    ? 'https://heros-market.vercel.app'
+    ? 'https://greatest-hero.vercel.app'
     : 'http://localhost:5173';
