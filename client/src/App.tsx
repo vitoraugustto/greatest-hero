36e87c826f9a7e0530fa203eba1282a9073edc04
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-27 23:00:35 -0300

    create a market demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-function App() {
-  const [count, setCount] = useState(0)
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+import './App.css';
+import reactLogo from './assets/react.svg';
+import { BACKEND_BASE_URL } from './config/api.js';
+
+interface IItem {
+  _id: string;
+  name: string;
+  image: string;
+  status: {
+    attack: number;
+    defense: number;
+  };
 }
 
-export default App
+interface IText {
+  fontSize?: number;
+  children: React.ReactNode;
+}
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  const fetchItems = () => {
+    axios
+      .get(`${BACKEND_BASE_URL}/api/v1/items`)
+      .then((res) => setItems(res.data));
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <div>
+      <VitePlusReact />
+      <h1>Hero's Market</h1>
+      {items.map((item: IItem) => (
+        <div
+          style={{
+            display: 'inline-block',
+            marginLeft: 12,
+            marginRight: 12,
+          }}
+          key={item._id}
+        >
+          <img style={{ width: 220, borderRadius: 12 }} src={item.image} />
+          <Text fontSize={18}>{item.name}</Text>
+          <Text fontSize={16}>Ataque: {item.status.attack}</Text>
+          <Text fontSize={16}>Defesa: {item.status.defense}</Text>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Text: React.FC<IText> = ({ fontSize, children }) => {
+  return (
+    <p
+      style={{
+        fontSize: fontSize,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        color: '#caa5fa',
+        letterSpacing: 0.5,
+      }}
+    >
+      {children}
+    </p>
+  );
+};
+
+const VitePlusReact = () => {
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+};
+
+export default App;
