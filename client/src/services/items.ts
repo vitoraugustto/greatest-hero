fbeb5234aceadc3454a94f86a850354297dcfc1a
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-09-30 00:11:19 -0300

    fix Module '../App' has no exported member 'IItem'

--- client/src/services/items.ts
+++ client/src/services/items.ts
@@ -1,6 +1,6 @@
 import { AxiosPromise } from 'axios';
 
-import { IItem } from '../App';
+import { IItem } from '../components/SlotItem/SlotItem';
 import { instance } from './axios';
 
 export const fetchItems = (): AxiosPromise<[IItem]> =>
