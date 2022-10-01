c7d717ded79d6f97ae03378bad649d315d85c72f
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-10-01 15:22:35 -0300

    add timestamps in Hero and Item models

--- server/src/models/Hero.js
+++ server/src/models/Hero.js
@@ -4,16 +4,19 @@ import { itemSchema } from './Item.js';
 
 const { Schema } = mongoose;
 
-const heroSchema = new Schema({
-  name: String,
-  role: String,
-  status: {
-    attack: Number,
-    defense: Number,
-    hp: Number,
+const heroSchema = new Schema(
+  {
+    name: String,
+    role: String,
+    status: {
+      attack: Number,
+      defense: Number,
+      hp: Number,
+    },
+    inventory: [itemSchema],
   },
-  inventory: [itemSchema],
-});
+  { timestamps: true }
+);
 
 const Hero = mongoose.model('Hero', heroSchema);
 
--- server/src/models/Item.js
+++ server/src/models/Item.js
@@ -2,16 +2,21 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-export const itemSchema = new Schema({
-  name: String,
-  description: String,
-  role: String,
-  image: String,
-  status: {
-    attack: Number,
-    defense: Number,
+export const itemSchema = new Schema(
+  {
+    name: String,
+    description: String,
+    role: String,
+    image: String,
+    status: {
+      attack: Number,
+      defense: Number,
+    },
+    gold: Number,
+    type: String,
   },
-});
+  { timestamps: true }
+);
 
 const Item = mongoose.model('Item', itemSchema);
 
