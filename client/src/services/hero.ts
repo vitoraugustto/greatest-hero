352f54904ce0040e9b34d9cb0dbde5eb4bfa81ce
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-10-04 00:59:11 -0300

    create inventory requests

--- client/src/services/hero.ts
+++ client/src/services/hero.ts
@@ -3,5 +3,11 @@ import { AxiosPromise } from 'axios';
 import { IItem } from '../components/SlotItem/SlotItem';
 import { instance } from './axios';
 
-export const storeInInventory = (itemId: IItem['_id']): AxiosPromise<[IItem]> =>
-  instance({ url: `api/v1/hero/inventory${itemId}`, method: 'POST' });
+export const fetchInventory = (): AxiosPromise<[IItem]> =>
+  instance({ url: 'api/v1/hero/inventory' });
+
+export const storeInInventory = (itemId: IItem['_id']): AxiosPromise =>
+  instance({ url: `api/v1/hero/inventory/${itemId}`, method: 'POST' });
+
+export const removeFromInventory = (itemId: IItem['_id']): AxiosPromise =>
+  instance({ url: `api/v1/hero/inventory/${itemId}`, method: 'PUT' });
