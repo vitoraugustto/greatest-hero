6fafa1b6a94dac9e78abe295297af90e28fb687c
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-11-10 01:11:48 -0300

    add ConfirmModal in HeroInventoryScreen

--- client/src/screens/HeroInventoryScreen.tsx
+++ client/src/screens/HeroInventoryScreen.tsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from 'react';
 
 import Background from '../components/Layout/Background';
+import Row from '../components/Layout/Row';
 import Spacer from '../components/Layout/Spacer';
+import { ConfirmModal } from '../components/Modals/ConfirmModal';
 import SlotItem, { IItem } from '../components/SlotItem/SlotItem';
 import Text from '../components/Text/Text';
 import { fetchInventory, removeFromInventory } from '../services/hero';
 
 export const HeroInventoryScreen = () => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<IItem>({
+    _id: '',
+    name: '',
+    image: '',
+    status: { attack: 0, defense: 0 },
+    gold: 0,
+    type: '',
+  });
+
   const [items, setItems] = useState<[IItem]>([
     {
       _id: '',
@@ -18,8 +30,27 @@ export const HeroInventoryScreen = () => {
     },
   ]);
 
+  const handleClick = (item: IItem): void => {
+    setSelectedItem(item);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => setModalOpen(false);
+
+  const handleFetchInventory = () => {
+    fetchInventory().then((res) => {
+      console.log('deu set');
+      setItems(res.data);
+    });
+  };
+
+  const sellItem = () => {
+    removeFromInventory(selectedItem._id).then(() => closeModal());
+    handleFetchInventory();
+  };
+
   useEffect(() => {
-    fetchInventory().then((res) => setItems(res.data));
+    handleFetchInventory();
   }, []);
 
   return (
@@ -28,23 +59,25 @@ export const HeroInventoryScreen = () => {
         Inventário
       </Text>
 
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-        }}
-      >
+      <Row flexWrap>
         {items.map((item: IItem) => (
           <Spacer key={item._id} mr={26} mt={26}>
-            <SlotItem
-              onClick={() => removeFromInventory(item._id)}
-              item={item}
-            />
+            <SlotItem onClick={() => handleClick(item)} item={item} />
           </Spacer>
         ))}
-      </div>
+      </Row>
+      <ConfirmModal
+        isModalOpen={isModalOpen}
+        onConfirm={sellItem}
+        onCancel={closeModal}
+      >
+        <Text size={20}>Você deseja vender o item "{selectedItem.name}"?</Text>
+        <Spacer mt={8} />
+        <Text color="gold">
+          Você receberá {selectedItem.gold} moedas de ouro.
+        </Text>
+        <Spacer mt={16} />
+      </ConfirmModal>
     </Background>
   );
 };
