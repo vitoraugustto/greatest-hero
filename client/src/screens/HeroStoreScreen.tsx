378cc9a3f6a1ea3328ab46246033e17832125874
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-11-10 00:49:14 -0300

    add ConfirmModal in HeroStoreScreen

--- client/src/screens/HeroStoreScreen.tsx
+++ client/src/screens/HeroStoreScreen.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import Modal from 'styled-react-modal';
 
 import Background from '../components/Layout/Background';
+import Row from '../components/Layout/Row';
 import Spacer from '../components/Layout/Spacer';
 import { Link } from '../components/Link/Link';
+import { ConfirmModal } from '../components/Modals/ConfirmModal';
 import SlotItem, { IItem } from '../components/SlotItem/SlotItem';
 import Text from '../components/Text/Text';
-import { Button } from '../components/UI/Button';
 import { storeInInventory } from '../services/hero';
 import { fetchItems } from '../services/items';
 
@@ -38,7 +38,6 @@ export const HeroStoreScreen = () => {
   const handleClick = (item: IItem): void => {
     setSelectedItem(item);
     setModalOpen(true);
-    // storeInInventory(itemId);
   };
 
   const closeModal = () => setModalOpen(false);
@@ -59,71 +58,25 @@ export const HeroStoreScreen = () => {
       <Link to="/hero/inventory">
         <Text>Ir para o inventário</Text>
       </Link>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-        }}
-      >
+      <Row wrap>
         {items.map((item: IItem) => (
           <Spacer key={item._id} mr={26} mt={26}>
             <SlotItem onClick={() => handleClick(item)} item={item} />
           </Spacer>
         ))}
-      </div>
-      <CustomModal
-        buyItem={buyItem}
-        closeModal={closeModal}
+      </Row>
+      <ConfirmModal
         isModalOpen={isModalOpen}
-        selectedItem={selectedItem}
-      />
-    </Background>
-  );
-};
-
-interface ICustomModal {
-  isModalOpen: boolean;
-  closeModal: () => void;
-  selectedItem: IItem;
-  buyItem: () => void;
-}
-
-const CustomModal: React.FC<ICustomModal> = ({
-  isModalOpen,
-  closeModal,
-  buyItem,
-  selectedItem,
-}) => {
-  return (
-    <StyledModal
-      onEscapeKeydown={closeModal}
-      onBackgroundClick={closeModal}
-      isOpen={isModalOpen}
-    >
-      <Spacer p={16}>
+        onConfirm={buyItem}
+        onCancel={closeModal}
+      >
         <Text size={20}>Você deseja comprar o item "{selectedItem.name}"?</Text>
         <Spacer mt={8} />
         <Text color="gold">
           Vai te custar {selectedItem.gold} moedas de ouro.
         </Text>
         <Spacer mt={16} />
-        <Button onClick={buyItem} borderColor="#caa5fa">
-          <Text>Sim</Text>
-        </Button>
-        <Spacer mt={8} />
-        <Button onClick={closeModal}>
-          <Text size={18}>Cancelar</Text>
-        </Button>
-      </Spacer>
-    </StyledModal>
+      </ConfirmModal>
+    </Background>
   );
 };
-
-const StyledModal = Modal.styled`
-  display: flex;
-  flex-direction: column;
-  border-radius: 8px;
-  background-color: #302a54;
-`;
