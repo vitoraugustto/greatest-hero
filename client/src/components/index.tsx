b3362bcfe4d9c8643d6a9151c89c4c07e4a60411
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-11-12 11:12:39 -0300

    create index components

---
+++ client/src/components/index.tsx
@@ -0,0 +1,11 @@
+import Background from './Layout/Background';
+import Box from './Layout/Box';
+import Row from './Layout/Row';
+import Spacer from './Layout/Spacer';
+import { Link } from './Link/Link';
+import SlotItem from './SlotItem/SlotItem';
+import { Text } from './Text';
+import { Button } from './UI/Button';
+import { Input } from './UI/Input';
+
+export { Spacer, Background, Box, Row, Link, SlotItem, Text, Button, Input };
