77583bc5da4880962f79ab66d37a33a3f3586490
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-11-12 11:08:58 -0300

    rename Input story

--- client/src/components/UI/Input/Input.stories.tsx
+++ client/src/components/UI/Input/Input.stories.tsx
@@ -18,4 +18,4 @@ export default {
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
-export const Default = Template.bind({});
+export const DefaultInput = Template.bind({});
