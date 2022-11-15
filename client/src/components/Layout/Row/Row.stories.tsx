a9977831380161fedd5f9baee9b510854807247e
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-11-15 13:48:47 -0300

    add decorators in Row's stories

--- client/src/components/Layout/Row/Row.stories.tsx
+++ client/src/components/Layout/Row/Row.stories.tsx
@@ -1,23 +1,37 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
+import { Text } from '../../Text';
 import { Box } from '../Box';
-import Spacer from '../Spacer/Spacer';
+import { Spacer } from '../Spacer';
 import { Row } from './Row';
 
 export default {
   title: 'Row',
   component: Row,
+  decorators: [
+    (Story) => (
+      <>
+        <Row vCenter>
+          <Box height={10} width={10} bgColor="pink" borderRadius={5} />
+          <Spacer mr={8} />
+          <Text color="pink">Row</Text>
+        </Row>
+        <Story />
+      </>
+    ),
+  ],
 } as ComponentMeta<typeof Row>;
 
 const Template: ComponentStory<typeof Row> = (args) => <Row {...args} />;
 
 export const _Row = Template.bind({});
 _Row.args = {
+  style: { backgroundColor: 'pink', height: 150 },
   children: (
     <>
-      <Box height={130} width={130} bgColor="blue" />
-      <Spacer mr={8} />
-      <Box height={70} width={70} bgColor="red" />
+      <Box height={100} width={100} bgColor="blue" />
+      <Spacer mr={16} />
+      <Box height={100} width={100} bgColor="red" />
     </>
   ),
 };
