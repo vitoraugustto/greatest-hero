1802dd6c61ecfbdd9e5d161fd3a4ec376e79fc77
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-12-05 17:01:55 -0300

    create equipItem and unequipItem services

--- client/src/services/hero.ts
+++ client/src/services/hero.ts
@@ -1,10 +1,17 @@
 import { AxiosPromise } from 'axios';
 
 import { IItem } from '../components/SlotItem/SlotItem.types';
+import { IHero } from '../hooks/useHero';
 import { instance } from './axios';
 
-export const fetchHero = (): AxiosPromise<{ inventory: IItem[] }> =>
+export const fetchHero = (): AxiosPromise<IHero> =>
   instance({ url: 'api/v1/hero' });
+
+export const equipItem = (itemId: IItem['_id']): AxiosPromise =>
+  instance({ url: `api/v1/hero/equip-item/${itemId}`, method: 'POST' });
+
+export const unequipItem = (itemId: IItem['_id']): AxiosPromise =>
+  instance({ url: `api/v1/hero/unequip-item/${itemId}`, method: 'PUT' });
 
 export const storeInInventory = (itemId: IItem['_id']): AxiosPromise =>
   instance({ url: `api/v1/hero/inventory/${itemId}`, method: 'POST' });
