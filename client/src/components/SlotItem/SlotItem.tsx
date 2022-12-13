3366be3e21634dd2a7bc77319fe298fac5276a17
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-12-13 16:07:53 -0300

    adjust SlotItem styles

--- client/src/components/SlotItem/SlotItem.tsx
+++ client/src/components/SlotItem/SlotItem.tsx
@@ -1,4 +1,5 @@
 import { Box } from '../Layout/Box';
+import { Row } from '../Layout/Row';
 import { Spacer } from '../Layout/Spacer';
 import { Text } from '../Text';
 import { ISlotItem } from './SlotItem.types';
@@ -12,24 +13,28 @@ export const SlotItem: React.FC<ISlotItem> = ({ item, onClick }) => {
       borderRadius={10}
       bgColor="#302a54"
       hCenter
+      width={260}
       onClick={onClick}
     >
       <Spacer p={16}>
-        <Text weight="bold" size={14}>
-          {name}
-        </Text>
-        <Box height={180} width={220} hCenter vCenter>
-          <img style={{ width: 120, borderRadius: 12 }} src={image} />
-        </Box>
-        <Box width={220}>
-          <Text size={14} color="gold">
-            {gold} moedas de ouro
+        <Box height={32}>
+          <Text weight="bold" size={14}>
+            {name}
           </Text>
-          <Spacer mt={10} />
-          <Text size={14}>Tipo: {type.toUpperCase()}</Text>
-          <Text size={14}>Ataque: {status.attack}</Text>
-          <Text size={14}>Defesa: {status.defense}</Text>
         </Box>
+        <img style={{ height: 160, borderRadius: 12 }} src={image} />
+        <Spacer mt={10} />
+        <Row>
+          <Box>
+            <Text size={14} color="gold">
+              {gold} moedas de ouro
+            </Text>
+            <Spacer mt={6} />
+            <Text size={14}>Tipo: {type.toUpperCase()}</Text>
+            <Text size={14}>Ataque: {status.attack}</Text>
+            <Text size={14}>Defesa: {status.defense}</Text>
+          </Box>
+        </Row>
       </Spacer>
     </Box>
   );
