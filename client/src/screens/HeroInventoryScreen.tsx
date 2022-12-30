7003ee1da00cbd2571ef18c5818af1c02d72ec84
Vitor Augusto <vitoraugusto002@hotmail.com>
2022-12-29 23:10:40 -0300

    add equippedItems and FloatingOptions to sell, equip or unequip an item

--- client/src/screens/HeroInventoryScreen.tsx
+++ client/src/screens/HeroInventoryScreen.tsx
@@ -1,40 +1,70 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   Background,
   Box,
   Button,
+  FloatingOptions,
   Link,
   Row,
   SlotItem,
   Spacer,
   Text,
 } from '../components';
+import { IOption } from '../components/FloatingOptions/FloatingOptions.types';
 import { ConfirmModal } from '../components/Modals/ConfirmModal';
 import { IItem } from '../components/SlotItem/SlotItem.types';
 import { useHero } from '../hooks/useHero';
 
 export const HeroInventoryScreen = () => {
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
-  const [selectedItem, setSelectedItem] = useState<IItem>(initItem);
-  const { inventory, sellItem } = useHero();
+  const [isConfirmModalOpen, setConfirmModalOpen] = useState<boolean>(false);
+  const [isFloatingOptionsVisible, setFloatingOptionsVisible] =
+    useState<boolean>(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [options, setOptions] = useState<IOption[]>([
+    { label: '', onClick: () => null },
+  ]);
+  const { inventory, sellItem, equipItem, unequipItem, equippedItems } =
+    useHero();
 
-  const closeModal = () => setModalOpen(false);
+  const handleSlotClick = (
+    item: IItem,
+    coords: { x: number; y: number }
+  ): void => {
+    selectedItem = item;
 
-  const handleClick = (item: IItem): void => {
-    setSelectedItem(item);
-    setModalOpen(true);
+    setCoords({ x: coords.x, y: coords.y });
+    setFloatingOptionsVisible(true);
   };
 
-  const handleSellItem = () => {
-    sellItem(selectedItem._id).then(closeModal);
+  const closeConfirmModal = () => setConfirmModalOpen(false);
+  const closeFloatingOptions = () => setFloatingOptionsVisible(false);
+
+  const handleEquipItem = () => {
+    equipItem(selectedItem._id).then(closeFloatingOptions);
   };
+
+  const handleUnequipItem = () => {
+    unequipItem(selectedItem._id).then(closeFloatingOptions);
+  };
+
+  const handleEsc = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') closeFloatingOptions();
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleEsc);
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
 
   return (
     <Background>
       <Box gap={18}>
         <Text align="center" as="h1" weight="bold" color="#fff">
-          Inventário
+          Herói
         </Text>
         <Row gap={18} hCenter>
           <Link to="/store">
@@ -44,20 +74,64 @@ export const HeroInventoryScreen = () => {
             <Button text="Menu" />
           </Link>
         </Row>
-        <Row gap={26} hCenter flexWrap>
-          {inventory.map((item) => (
-            <SlotItem
-              key={item._id}
-              onClick={() => handleClick(item)}
-              item={item}
-            />
-          ))}
+        <Row gap={18}>
+          <Inventory
+            inventory={inventory}
+            onSlotClick={(data) => {
+              setOptions([{ label: 'Equipar', onClick: handleEquipItem }]);
+              handleSlotClick(data.item, {
+                x: data.e.clientX,
+                y: data.e.clientY,
+              });
+            }}
+          />
+          <Box hCenter flex>
+            <Text align="center" size={20} weight="bold">
+              Equipados
+            </Text>
+            <Spacer mt={16} />
+            <EmptySlot text="Cabeça" />
+            <Spacer mt={8} />
+            <Row gap={8} hCenter>
+              <EmptySlot text="Mão direita" />
+              <EmptySlot text="Peitoral" />
+              <EmptySlot text="Mão esquerda" />
+            </Row>
+            <Spacer mt={8} />
+            <Row gap={8} hCenter>
+              <EmptySlot text="Mãos" />
+              <EmptySlot text="Pernas" />
+              {equippedItems.find((item) => item.type === 'botas') ? (
+                <SlotItem
+                  key={equippedItems.find((item) => item.type === 'botas')?._id}
+                  size="small"
+                  infos={false}
+                  onClick={(e) => {
+                    handleSlotClick(
+                      equippedItems.find((item) => item.type === 'botas') ||
+                        initItem,
+                      { x: e.clientX, y: e.clientY }
+                    );
+                    setOptions([
+                      { label: 'Desequipar', onClick: handleUnequipItem },
+                    ]);
+                  }}
+                  item={
+                    equippedItems.find((item) => item.type === 'botas') ||
+                    initItem
+                  }
+                />
+              ) : (
+                <EmptySlot text="Pés" />
+              )}
+            </Row>
+          </Box>
         </Row>
       </Box>
       <ConfirmModal
-        isModalOpen={isModalOpen}
-        onConfirm={handleSellItem}
-        onCancel={closeModal}
+        isModalOpen={isConfirmModalOpen}
+        onConfirm={() => sellItem(selectedItem._id).then(closeConfirmModal)}
+        onCancel={closeConfirmModal}
       >
         <Text size={20}>Você deseja vender o item "{selectedItem.name}"?</Text>
         <Spacer mt={8} />
@@ -66,7 +140,55 @@ export const HeroInventoryScreen = () => {
         </Text>
         <Spacer mt={16} />
       </ConfirmModal>
+      <FloatingOptions
+        x={coords.x}
+        y={coords.y}
+        isVisible={isFloatingOptionsVisible}
+        options={options}
+      />
     </Background>
+  );
+};
+
+const EmptySlot = ({ text }: { text: string }) => {
+  return (
+    <Box
+      hCenter
+      vCenter
+      height={154}
+      width={180}
+      borderRadius={8}
+      bgColor="#302a54"
+    >
+      <Text>{text}</Text>
+    </Box>
+  );
+};
+
+interface IInventory {
+  inventory: IItem[];
+  onSlotClick: ({ e, item }: { e: MouseEvent; item: IItem }) => void;
+}
+
+const Inventory: React.FC<IInventory> = ({ inventory, onSlotClick }) => {
+  return (
+    <Box>
+      <Text align="center" size={20} weight="bold">
+        Inventário
+      </Text>
+      <Spacer mt={16} />
+      <Row gap={26} hCenter flexWrap>
+        {inventory.map((item) => (
+          <SlotItem
+            key={item._id}
+            size="small"
+            infos={false}
+            onClick={(e) => onSlotClick({ e, item })}
+            item={item}
+          />
+        ))}
+      </Row>
+    </Box>
   );
 };
 
@@ -78,3 +200,5 @@ const initItem = {
   gold: 0,
   type: '',
 };
+
+let selectedItem: IItem = initItem;
