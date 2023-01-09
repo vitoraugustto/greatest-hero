070ef143a64a3e3bd59a22e7317de6ad40a6268c
vitoraugustto <vitoraugusto002@hotmail.com>
2023-01-09 11:08:36 -0300

    lint store.ts

--- client/src/services/store.ts
+++ client/src/services/store.ts
@@ -1,8 +1,7 @@
 import { AxiosPromise } from 'axios';
 
-import { IItem } from "../components/SlotItem/SlotItem.types";
+import { IItem } from '../components/SlotItem/SlotItem.types';
 import { instance } from './axios';
 
-export const purchaseItem = (itemId: IItem ['_id']): AxiosPromise =>
+export const purchaseItem = (itemId: IItem['_id']): AxiosPromise =>
   instance({ url: `/api/v1/store/${itemId}/purchase`, method: 'POST' });
-
