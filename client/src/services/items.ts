8226133583b154c639cf5d922986fd644c2d7e13
vitoraugustto <vitoraugusto002@hotmail.com>
2023-01-12 23:00:52 -0300

    rename from 'fetchItems' to 'fetchStore' and move to 'store.ts'

--- client/src/services/items.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosPromise } from 'axios';
-
-import { IItem } from '../components/SlotItem/SlotItem.types';
-import { instance } from './axios';
-
-export const fetchItems = (): AxiosPromise<IItem[]> =>
-  instance({ url: '/api/v1/items' });
--- client/src/services/store.ts
+++ client/src/services/store.ts
@@ -3,5 +3,8 @@ import { AxiosPromise } from 'axios';
 import { IItem } from '../components/SlotItem/SlotItem.types';
 import { instance } from './axios';
 
+export const fetchStore = (): AxiosPromise<IItem[]> =>
+  instance({ url: '/api/v1/items' });
+
 export const purchaseItem = (itemId: IItem['_id']): AxiosPromise =>
   instance({ url: `/api/v1/store/${itemId}/purchase`, method: 'POST' });
