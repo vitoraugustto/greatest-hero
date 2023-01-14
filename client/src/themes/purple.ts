149c6c32ebb0dbbd7755043e5cf8db4123a30848
vitoraugustto <vitoraugusto002@hotmail.com>
2023-01-14 01:05:21 -0300

    create purple theme

---
+++ client/src/themes/purple.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+export const purple: DefaultTheme = {
+  name: 'Tema padrão',
+  colors: {
+    background: { emphasized: '#302a54', notEmphasized: '#332e5b' },
+    font: '#caa5fa',
+    border: '#caa5fa',
+    title: '#fff',
+    gold: '#ffd111',
+  },
+};
