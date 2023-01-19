3316d2f456c655b983152381b751227dd105c58f
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-01-18 23:55:46 -0300

    add modals in components/index

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -5,6 +5,8 @@ import { Box } from './Layout/Box';
 import { Row } from './Layout/Row';
 import { Spacer } from './Layout/Spacer';
 import { Link } from './Link';
+import { ConfirmModal } from './Modals/ConfirmModal';
+import { ModalBase } from './Modals/ModalBase';
 import { SlotItem } from './SlotItem';
 import { Text } from './Text';
 import { Toast } from './Toast';
@@ -24,4 +26,6 @@ export {
   FloatingOptions,
   Toast,
   Aside,
+  ModalBase,
+  ConfirmModal,
 };
