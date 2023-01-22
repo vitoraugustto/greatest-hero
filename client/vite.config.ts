3f0e26905dfc36d303363b36bfd075264a6c4ec9
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-01-21 23:57:31 -0300

    remove 'vite-plugin-svgr'

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,13 +2,12 @@
 /// <reference types="vite/client" />
 import react from '@vitejs/plugin-react';
 import { InlineConfig, UserConfig, defineConfig } from 'vite';
-import viteSvgr from 'vite-plugin-svgr';
 
 interface VitestConfigExport extends UserConfig {
   test: InlineConfig;
 }
 
 export default defineConfig({
-  plugins: [react(), viteSvgr()],
+  plugins: [react()],
   test: { globals: true, environment: 'jsdom' },
 } as VitestConfigExport);
