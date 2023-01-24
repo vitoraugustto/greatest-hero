3128f81910ac2c684354ca4d9a8cd06bbaad5d3a
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-01-23 23:53:27 -0300

    add redux 'Provider'

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { App } from './App';
+import { store } from './store/index';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
