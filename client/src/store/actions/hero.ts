418630ce13db141821dd62bcdb0a17bbf7a2e66d
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-01-27 17:00:42 -0300

    remove unused 'async' from 'fetchHeroAction'

--- client/src/store/actions/hero.ts
+++ client/src/store/actions/hero.ts
@@ -2,6 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { fetchHero } from '../../services/hero';
 
-export const fetchHeroAction = createAsyncThunk('hero/fetchHero', async () =>
+export const fetchHeroAction = createAsyncThunk('hero/fetchHero', () =>
   fetchHero().then((res) => res.data)
 );
