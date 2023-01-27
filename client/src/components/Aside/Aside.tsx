85429e79d72fea3f15607ca5564fbb230c32aea2
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-01-27 01:43:15 -0300

    add hero infos in 'Aside' component

--- client/src/components/Aside/Aside.tsx
+++ client/src/components/Aside/Aside.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 import Armor from '../../assets/icons/armor.png';
 import Chest from '../../assets/icons/chest.png';
 import Store from '../../assets/icons/store.png';
 import { ASIDE_WIDTH } from '../../common/constants';
+import { useHero } from '../../hooks/useHero';
 import { Box } from '../Layout/Box';
 import { Row } from '../Layout/Row';
 import { Spacer } from '../Layout/Spacer';
@@ -13,9 +15,20 @@ import { StyledAside } from './Aside.styles';
 import { IAside, IAsideOption } from './Aside.types';
 
 export const Aside: React.FC<IAside> = ({ children }) => {
+  const { hero } = useHero();
+  const theme = useTheme();
+
   return (
     <>
       <StyledAside>
+        <Spacer pl={24}>
+          <Text>
+            {hero.name}, {hero.role}
+          </Text>
+          <Text color={theme.colors.font.gold}>
+            Moedas de ouro: {hero.gold}
+          </Text>
+        </Spacer>
         <AsideOption linkTo="/" label="Equipamento" icon={Armor} />
         <AsideOption linkTo="/hero/inventory" label="Inventário" icon={Chest} />
         <AsideOption linkTo="/store" label="Loja" icon={Store} />
