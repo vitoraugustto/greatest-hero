76e796a0e5adf251c6e4dbb5c74e8cc2c5dd210f
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-02-02 16:01:20 -0300

    chore: add alias paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,22 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import react from '@vitejs/plugin-react';
+import path from 'path';
 import { InlineConfig, UserConfig, defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 interface VitestConfigExport extends UserConfig {
   test: InlineConfig;
 }
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@common': path.resolve(__dirname, './src/common/'),
+      '@hooks': path.resolve(__dirname, './src/hooks/'),
+    },
+  },
   plugins: [react()],
   test: { globals: true, environment: 'jsdom' },
 } as VitestConfigExport);
