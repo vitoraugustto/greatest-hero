2cdf79df0d166a5b74e98ca77b4749766f89a8dc
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-02-02 01:34:54 -0300

    chore: create 'defaultTheme'

---
+++ client/src/themes/default/colors.ts
@@ -0,0 +1,25 @@
+import { Color } from '../../common/interfaces';
+
+interface PaletteColor {
+  300: Color;
+  main: Color;
+  500: Color;
+  600: Color;
+  700: Color;
+}
+
+export const violet: PaletteColor = {
+  300: '#ddc5fc',
+  main: '#caa5fa',
+  500: '#c69ff9',
+  600: '#bb8bf8',
+  700: '#af78f7',
+};
+
+export const purple: PaletteColor = {
+  300: '#3F376D',
+  main: '#302a54',
+  500: '#272244',
+  600: '#1f1b36',
+  700: '#181529',
+};
---
+++ client/src/themes/default/theme.ts
@@ -0,0 +1,46 @@
+import { DefaultTheme } from 'styled-components';
+
+import { purple, violet } from './colors';
+
+export const defaultTheme: DefaultTheme = {
+  name: 'Tema padrão',
+  typography: {
+    body: {
+      color: '#caa5fa',
+      fontFamily: 'Courier New',
+      fontSize: '1em',
+      fontWeight: 'normal',
+      letterSpacing: '0px',
+    },
+    title: {
+      color: '#fff',
+      fontFamily: 'Courier New',
+      fontSize: '26px',
+      fontWeight: 'bold',
+      letterSpacing: '0px',
+    },
+    button: {
+      color: '#caa5fa',
+      fontFamily: 'Courier New',
+      fontSize: '22px',
+      fontWeight: 'bold',
+      letterSpacing: '2px',
+    },
+  },
+  palette: {
+    primary: {
+      300: violet[300],
+      main: violet.main,
+      500: violet[500],
+      600: violet[600],
+      700: violet[700],
+    },
+    secondary: {
+      300: purple[300],
+      main: purple.main,
+      500: purple[500],
+      600: purple[600],
+      700: purple[700],
+    },
+  },
+};
