2a1254b52955c01702dd7841a6472bca165bb535
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-02-07 00:06:56 -0300

    chore: setup tsconfigPaths with storybook

--- client/.storybook/main.cjs
+++ client/.storybook/main.cjs
@@ -1,18 +1,22 @@
+const tsconfigPaths = require('vite-tsconfig-paths');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    config.plugins.push(tsconfigPaths.default());
+
+    return config;
+  },
+};
