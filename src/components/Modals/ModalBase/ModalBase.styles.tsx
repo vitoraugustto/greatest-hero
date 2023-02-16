57b6a2323016f7af55b10ca3f153da7da7f3c760
Vitor Augusto <60718553+vitoraugustto@users.noreply.github.com>
2023-02-15 23:34:48 -0300

    style: add indentation

--- src/components/Modals/ModalBase/ModalBase.styles.tsx
+++ src/components/Modals/ModalBase/ModalBase.styles.tsx
@@ -2,9 +2,9 @@ import { DefaultTheme } from 'styled-components';
 import Modal from 'styled-react-modal';
 
 export const StyledModal = Modal.styled`
-border-radius: 8px;
-background-color: ${(props: { theme: DefaultTheme }) =>
-  props.theme.palette.secondary.main};
-border: 2px solid ${(props: { theme: DefaultTheme }) =>
-  props.theme.palette.primary.main}
+  border-radius: 8px;
+  background-color: ${(props: { theme: DefaultTheme }) =>
+    props.theme.palette.secondary.main};
+  border: 2px solid ${(props: { theme: DefaultTheme }) =>
+    props.theme.palette.primary.main}
 `;
