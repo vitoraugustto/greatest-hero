e259b349a14b21260adecfaad46bc7cb3b218db8
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-02-28 23:37:17 -0300

    feat: change Row types

--- src/components/Layout/Row/Row.types.ts
+++ src/components/Layout/Row/Row.types.ts
@@ -1,11 +1,13 @@
 import { CSSObject } from 'styled-components';
 
+import { Color } from '@common/interfaces';
+
 export interface IRow {
   vCenter?: boolean;
   hCenter?: boolean;
   flexWrap?: boolean;
-  gap?: number;
-  bgColor?: string;
+  gap?: number | string;
+  bgColor?: Color;
   style?: CSSObject;
   children: React.ReactElement | React.ReactElement[];
 }
