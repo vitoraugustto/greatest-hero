914d71ea181a001d170ea14d330ce2aba9fbeeea
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-20 23:51:21 -0300

    feat: create fetchEnemy

--- src/services/enemy.ts
+++ src/services/enemy.ts
@@ -5,3 +5,6 @@ import { instance } from '@services/axios';
 
 export const fetchEnemies = (): AxiosPromise<IEnemy[]> =>
   instance({ url: '/api/v1/enemies' });
+
+export const fetchEnemy = (id: IEnemy['_id']): AxiosPromise<IEnemy> =>
+  instance({ url: `/api/v1/enemies/${id}` });
