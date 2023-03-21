1543f15d7a6d47d97aa7d642991feb4583c1c142
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-20 23:42:10 -0300

    feat: add Link to /combat/{{enemyId}}/fight

--- src/screens/Combat/EnemiesScreen.tsx
+++ src/screens/Combat/EnemiesScreen.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useTheme } from 'styled-components';
 
 import { IEnemy } from '@common/interfaces';
-import { Aside, Background, Box, Row, Spacer, Text } from '@components';
+import { Aside, Background, Box, Link, Row, Spacer, Text } from '@components';
 import { fetchEnemies } from '@services/enemy';
 
 export const EnemiesScreen = () => {
@@ -28,12 +28,14 @@ export const EnemiesScreen = () => {
         <React.Fragment>
           {enemies.map((enemy) => (
             <Row key={enemy._id}>
-              <Box borderRadius={10} bgColor={theme.palette.secondary.main}>
-                <Spacer p={10}>
-                  <img src={enemy.image} width="124px" />
-                  <Text>{enemy.name}</Text>
-                </Spacer>
-              </Box>
+              <Link to={`/combat/${enemy._id}/fight`}>
+                <Box borderRadius={10} bgColor={theme.palette.secondary.main}>
+                  <Spacer p={10}>
+                    <img src={enemy.image} width="124px" />
+                    <Text>{enemy.name}</Text>
+                  </Spacer>
+                </Box>
+              </Link>
             </Row>
           ))}
         </React.Fragment>
